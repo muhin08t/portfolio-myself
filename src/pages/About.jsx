a6d86ad292350e301ffe349c4e9750dfea3509f7
Mohiuddin <muhin08t@gmail.com>
2024-09-24 12:06:08 +0600

    about and blog page implemented

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,25 @@
-// About.js
+import React from 'react';
+
 const About = () => {
-    return <div>About</div>;
-}
+    return (
+        <div className='py-10 px-32 min-h-2/3'>
+             <h1 className="mx-auto text-center font-bold text-3xl pb-3">About us</h1>
+             <p className='py-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                 Molestias in non blanditiis ipsam consequuntur voluptatem? 
+                 Tempora facere voluptatem nemo expedita possimus. Repellat 
+                 saepe nemo beatae iste impedit eveniet quasi sit.
+                 Molestias in non blanditiis ipsam consequuntur voluptatem? 
+                 Tempora facere voluptatem nemo expedita possimus. Repellat 
+                 saepe nemo beatae iste impedit eveniet quasi sit.
+                 Molestias in non blanditiis ipsam consequuntur voluptatem? 
+                 Tempora facere voluptatem nemo expedita possimus. Repellat 
+                 saepe nemo beatae iste impedit eveniet quasi sit.
+                 Molestias in non blanditiis ipsam consequuntur voluptatem? 
+                 Tempora facere voluptatem nemo expedita possimus. Repellat 
+                 saepe nemo beatae iste impedit eveniet quasi sit.
+                 </p>
+        </div>
+    );
+};
 
 export default About;
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            Blog 
+        <div className='py-10 px-32 min-h-2/3'>
+             <h1 className="mx-auto text-center font-bold text-3xl pb-3">Blog section</h1>
+             <p className='py-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                 Molestias in non blanditiis ipsam consequuntur voluptatem? 
+                 Tempora facere voluptatem nemo expedita possimus. Repellat 
+                 saepe nemo beatae iste impedit eveniet quasi sit.
+                 Molestias in non blanditiis ipsam consequuntur voluptatem? 
+                 Tempora facere voluptatem nemo expedita possimus. Repellat 
+                 saepe nemo beatae iste impedit eveniet quasi sit.
+                 Molestias in non blanditiis ipsam consequuntur voluptatem? 
+                 Tempora facere voluptatem nemo expedita possimus. Repellat 
+                 saepe nemo beatae iste impedit eveniet quasi sit.
+                 Molestias in non blanditiis ipsam consequuntur voluptatem? 
+                 Tempora facere voluptatem nemo expedita possimus. Repellat 
+                 saepe nemo beatae iste impedit eveniet quasi sit.
+                 </p>
         </div>
     );
 };
